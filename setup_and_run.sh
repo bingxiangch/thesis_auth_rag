ae879ebdec1db358f43a55fc39818c179e25e2cf
bingxiang.chen <bingxiang.chen@tuni.fi>
2024-04-29 00:27:22 +0300

    script

--- setup_and_run.sh
+++ setup_and_run.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Function to write values to config.yaml
+write_to_yaml() {
+    cat <<EOF >backend/setting.yaml
+access_token: $hf_token
+EOF
+}
+# Check if hf_token argument is provided
+if [ -z "$1" ]; then
+    echo "Error: hf_token argument is missing."
+    exit 1
+else
+    hf_token=$1
+fi
+
 # Backend setup
 cd backend && python3.11 -m venv .venv && source .venv/bin/activate && \
 pip install --upgrade pip poetry && poetry install --with local && poetry install --extras chroma && ./scripts/setup
--- setup_and_run_GPU.sh
+++ setup_and_run_GPU.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+# Function to write values to config.yaml
+write_to_yaml() {
+    cat <<EOF >backend/setting.yaml
+access_token: $hf_token
+EOF
+}
+# Check if hf_token argument is provided
+if [ -z "$1" ]; then
+    echo "Error: hf_token argument is missing."
+    exit 1
+else
+    hf_token=$1
+fi
+
 # Backend setup
-cd backend && export PYTHONPATH="${PYTHONPATH}:$(pwd)" && python3.11 -m venv .venv && source .venv/bin/activate && \
+cd backend  && python3.11 -m venv .venv && source .venv/bin/activate && \
 pip install --upgrade pip poetry && poetry install --with local && poetry install --extras chroma && ./scripts/setup
 
 CMAKE_ARGS='-DLLAMA_CUBLAS=on' poetry run pip install --force-reinstall --no-cache-dir llama-cpp-python
