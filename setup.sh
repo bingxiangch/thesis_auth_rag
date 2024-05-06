785e4c15a9d852f64a7b32466ea5c7fc075d709a
bingxiang.chen <bingxiang.chen@tuni.fi>
2024-05-06 16:58:03 +0300

    setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ cd backend && python3.11 -m venv .venv && source .venv/bin/activate
 poetry run python3.11 -m auth_RAG &
 
 # Frontend setup
-cd ../frontend && npm install
+cd ../frontend
 
 # Start the frontend server
 npm start
